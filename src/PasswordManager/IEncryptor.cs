3437803cf9cf2e9dc027733361d4edc1a1ec8d9d
Mustafa Arslan <mustafa.arslan1992@gmail.com>
2021-06-10 15:51:10 +0300

    Create IEncryptor.cs

---
+++ src/PasswordManager/IEncryptor.cs
@@ -0,0 +1,8 @@
+﻿namespace PasswordManager
+{
+    public interface IEncryptor
+    {
+        string Encrypt(string text, string password);
+        string Decrypt(string text, string password);
+    }
+}
