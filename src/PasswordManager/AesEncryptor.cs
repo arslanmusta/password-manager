84de11c116a5f163c94ed16d77312e4cb34bbce1
Mustafa Arslan <mustafa.arslan1992@gmail.com>
2021-06-10 17:10:13 +0300

    Create AesEncryptor.cs

---
+++ src/PasswordManager/AesEncryptor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordManager
+{
+    public class AesEncryptor : IEncryptor
+    {
+        public string Encrypt(string text, string password)
+        {
+            using var aes = Aes.Create();
+
+            aes.Key = Encoding.UTF8.GetBytes(password);
+            aes.IV = new byte[16];
+            
+            using var memoryStream = new MemoryStream();
+
+            using var cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+            using var streamWriter = new StreamWriter(cryptoStream);
+
+            streamWriter.Write(text);
+
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+
+        public string Decrypt(string text, string password)
+        {
+            using var aes = Aes.Create();
+            aes.Key = Encoding.UTF8.GetBytes(password);
+            aes.IV = new byte[16];
+            
+            using var memoryStream = new MemoryStream(Convert.FromBase64String(text));
+            using var cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            using var streamReader = new StreamReader(cryptoStream);
+
+            return streamReader.ReadToEnd();
+        }
+    }
+}
