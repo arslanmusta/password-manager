c19beb6a16bf8327f286bd3d5c630988f2ded458
Mustafa Arslan <mustafa.arslan1992@gmail.com>
2021-10-02 21:03:15 +0300

    new program.cs

--- src/PasswordManager/Program.cs
+++ src/PasswordManager/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PasswordManager.Commands;
 
 namespace PasswordManager
 {
@@ -6,7 +7,33 @@ namespace PasswordManager
     {
         private static void Main(string[] args)
         {
-            
+            try
+            {
+                var commandName = args[0];
+                var commandBuilder = new CommandBuilder();
+
+                for (var i = 2; i < args.Length; i += 2)
+                {
+                    commandBuilder.SetParam(args[i - 1][1..], args[i]);
+                }
+
+                commandBuilder.SetCommand(commandName);
+
+                var command = commandBuilder.Build();
+
+                if (command != null)
+                {
+                    command.Execute();
+                }
+                else
+                {
+                    Console.WriteLine($"wrong command name: {commandName}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occured while executing command: {e.Message}");
+            }
         }
     }
 }
