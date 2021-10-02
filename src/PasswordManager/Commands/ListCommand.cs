5727edcc6e7f4f7bb442e47a32ee3c987f34eb76
Mustafa Arslan <mustafa.arslan1992@gmail.com>
2021-10-02 21:02:42 +0300

    List command

---
+++ src/PasswordManager/Commands/ListCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PasswordManager.Commands
+{
+    public class ListCommand : IFileCommand, IMasterPasswordCommand
+    {
+        public string FileName { get; set; }
+        public string MasterPassword { get; set; }
+        
+        public void Execute()
+        {
+            var repository = new PasswordFileRepository( FileName, new AesEncryptor(), MasterPassword);
+            var passwords = repository.GetAll();
+            foreach (var password in passwords)
+            {
+                Console.WriteLine($"Domain: {password.Domain}, Password: {password.HashedPassword}");
+            }
+        }
+
+    }
+}
