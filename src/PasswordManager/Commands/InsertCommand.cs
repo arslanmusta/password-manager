1619a1d1ada524921cad67846327852cea870958
Mustafa Arslan <mustafa.arslan1992@gmail.com>
2021-10-02 21:02:33 +0300

    Insert command

---
+++ src/PasswordManager/Commands/InsertCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace PasswordManager.Commands
+{
+    public class InsertCommand : IFileCommand, IMasterPasswordCommand, IPasswordCommand, IDomainCommand
+    {
+        public string FileName { get; set; }
+        public string MasterPassword { get; set; }
+        public string Password { get; set; }
+        public string Domain { get; set; }
+        
+        public void Execute()
+        {
+            var repository = new PasswordFileRepository(FileName, new AesEncryptor(), MasterPassword);
+            repository.Add(new Password
+            {
+                Domain = Domain,
+                HashedPassword = Password
+            });
+        }
+    }
+}
